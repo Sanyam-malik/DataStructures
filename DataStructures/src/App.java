f565116b1aa8ad69f112a20114822d5ecd8f5ac7
Half Devil <the.halfdevils@gmail.com>
2023-07-24 04:28:19 +0530

    MISC: Implemented ArrayList

--- DataStructures/src/App.java
+++ DataStructures/src/App.java
@@ -1,17 +1,31 @@
 import java.util.List;
 
+import basics.ArrayList;
 import basics.LinkedList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        LinkedList<String> s = new LinkedList<>();
-        s.add("Arjun");
-        s.add("Ridhi");
-        s.add("Tanya");
-        s.add("Chiku");
-        s.addAll(List.of("Slvia", "Ronald"));
-        s.remove("Tanya");
-        System.out.println(s);
+        System.out.println("ArrayList!");
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Arjun");
+        al.add("Ridhi");
+        al.add("Tanya");
+        al.add("Chiku");
+        al.addAll(List.of("Slvia", "Ronald"));
+        al.remove("Tanya");
+        System.out.println(al);
+
+
+        System.out.println("LinkedList!");
+        LinkedList<String> ll = new LinkedList<String>();
+        ll.add("Arjun");
+        ll.add("Ridhi");
+        ll.add("Tanya");
+        ll.add("Chiku");
+        ll.addAll(List.of("Slvia", "Ronald"));
+        ll.remove("Tanya");
+        System.out.println(ll);
+
+
     }
 }
---
+++ DataStructures/src/basics/ArrayList.java
@@ -0,0 +1,102 @@
+package basics;
+
+import java.util.List;
+
+public class ArrayList<T> {
+    
+    private T array[];
+    private int size;
+
+    public ArrayList() {
+        array = (T[])new Object[10];
+    }
+
+    public void add(T data){
+        if(size == array.length) {
+            int newSize = (array.length + (array.length/2)) + 1;
+            createNewArray(newSize);
+            array[size] = data;
+            size = size + 1;
+        } else {
+            array[size] = data;
+            size = size + 1;
+        }
+    }
+
+    private void createNewArray(int size){
+        T newArray[] = (T[])new Object[size];
+        for(int index = 0 ; index <= array.length - 1 ; index++){
+            newArray[index] = array[index];
+        }
+        array = newArray;
+    }
+
+    public void addAll(List<T> dataList){
+        for(T data: dataList){
+            this.add(data);
+        }
+    }
+
+    public void remove(T data){
+        int index = getIndexOfElement(data);
+        if(index > -1){
+            this.remove(index);
+        }
+    }
+
+    private int getIndexOfElement(T data){
+        for(int index = 0; index < size; index++) {
+            if(data == array[index]) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public void remove(int index){
+        if(index < size){
+            for(int ei = index; ei < size; ei++){
+                array[ei] = array[ei+1];
+            }
+            size--;
+        }
+    }
+
+    public T get(int index){
+        if(index < size){
+            return array[index];
+        } else {
+            return null;
+        }
+    }
+
+    public boolean contains(T data){
+        for(int index = 0; index < size; index++) {
+            if(data == array[index]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clear(){
+        array = (T[])new Object[10];
+        size = 0;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int index = 0; index < size; index++) {
+            sb.append(array[index].toString());
+            if(index != size && index < size-1){
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
+}
