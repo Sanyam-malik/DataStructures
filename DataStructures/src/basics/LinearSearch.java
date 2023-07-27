2748ac9d33b39740ea4bfae699c11fa5a73f2af9
Sanyam Malik <sanyam.malik@clayhr.com>
2023-07-27 14:33:45 +0530

    MISC: Implemented Linear Search

--- DataStructures/src/basics/LinearSearch.java
+++ DataStructures/src/basics/LinearSearch.java
@@ -1,4 +1,19 @@
 package basics;
-public class LinearSearch {
-    
+public class LinearSearch<T extends Number> {
+    Collections<? extends Number> collection;
+    String type;
+
+    public LinearSearch(Collections<? extends Number> collection) {
+        this.collection = collection;
+    }
+
+    public int contains(T data) {
+        int lastIndex = this.collection.size();
+        for(int current=0 ; current < lastIndex; current++) {
+            if(this.collection.get(current) == data){
+                return current;
+            }
+        }
+        return -1;
+    }
 }
