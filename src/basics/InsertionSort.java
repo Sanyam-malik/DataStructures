c71ed5b527cd51a15aead80b4f94375a73a59e26
Sam Arora <samarora1051@gmail.com>
2023-08-05 22:24:58 +0530

    MISC: Emptied Code For Revision

--- src/basics/InsertionSort.java
+++ src/basics/InsertionSort.java
@@ -1,45 +1,4 @@
 package basics;
-public class InsertionSort<E extends Number> {
-    public static final String TYPE_ASCENDING = "ASCENDING";
+public class InsertionSort {
 
-    public static final String TYPE_DESCENDING = "DESCENDING";
-
-    Collections<E> list;
-    String type;
-
-    public InsertionSort(Collections<E> list){
-        this.list = list;
-        this.type = TYPE_ASCENDING;
-    }
-
-    
-    public InsertionSort(Collections<E> list, String type){
-        this.list = list;
-        this.type = type;
-    }
-
-    public void sort(){
-        int lastIndex = list.size();
-        for(int current=1 ; current < lastIndex; current++) {
-            int prev = current - 1;
-            E currData = list.get(current);
-            E prevData = list.get(prev);
-
-            if(this.type.equalsIgnoreCase(TYPE_ASCENDING)){
-                while(prev >=0 && currData.doubleValue() < prevData.doubleValue()){
-                    list.set(prev+1, list.get(prev));
-                    prevData = prev-1 >=0 ? list.get(prev-1) : null;
-                    prev--;
-                }
-                list.set(prev+1, currData);
-            } else {
-                while(prev >=0 && currData.doubleValue() > prevData.doubleValue()){
-                    list.set(prev+1, list.get(prev));
-                    prevData = prev-1 >=0 ? list.get(prev-1) : null;
-                    prev--;
-                }
-                list.set(prev+1, currData);
-            }
-        }
-    }
 }
