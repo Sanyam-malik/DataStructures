a7cc0afac0acbcfee2f8be579f1727e9a6db9493
Sam Arora <samarora1051@gmail.com>
2023-08-22 12:56:28 +0530

    MISC: Added Rev1 Stack and Queue Optimized Implementation

--- src/basics/Queue.java
+++ src/basics/Queue.java
@@ -1,4 +1,35 @@
 package basics;
-public class Queue {
+public class Queue<T> {
+
+    LinkedList<T> linkedList = new LinkedList<>();
+
+    public T peek(){
+        return linkedList.get(0);
+    }
+
+    public void push(T data) {
+        linkedList.add(data);
+    }
+
+    public T pop(){
+        return linkedList.remove(0);
+    }
+
+    public String toString(){
+        return linkedList.toString();
+    }
+
+    public static void main(String args[]){
+        Queue<Integer> linkList = new Queue<>();
+        linkList.push(10);
+        linkList.push(40);
+        linkList.push(80);
+        linkList.push(20);
+        linkList.push(60);
+        System.out.println(linkList);
+        System.out.println(linkList.peek());
+        System.out.println(linkList.pop());
+        System.out.println(linkList);
+    }
     
 }
--- src/basics/Stack.java
+++ src/basics/Stack.java
@@ -1,5 +1,35 @@
 package basics;
 
-public class Stack {
+public class Stack<T> {
+
+    ArrayList<T> arrList = new ArrayList<>();
+
+    public T peek(){
+        return arrList.get(arrList.size-1);
+    }
+
+    public void push(T data) {
+        arrList.add(data);
+    }
+
+    public T pop(){
+        return arrList.remove(arrList.size -1);
+    }
+
+    public String toString(){
+        return arrList.toString();
+    }
+
+    public static void main(String args[]){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(40);
+        stack.push(80);
+        stack.push(20);
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+    }
 
 }
