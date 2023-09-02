2a0d3bcbbb3c50583cecaf778e8cf382d989267e
Sam Arora <samarora1051@gmail.com>
2023-09-02 16:18:31 +0530

    MISC: Added Tree Rev1 Implementation

--- src/basics/Trie.java
+++ src/basics/Trie.java
@@ -2,4 +2,49 @@ package basics;
 
 public class Trie  {
 
+    Node root = new Node();
+
+    public class Node {
+        Node children[] = new Node[26];
+        boolean eow;
+
+        Node(){
+            for(int i=0; i<children.length; i++) {
+                this.children[i] = null;
+            }
+        }
+    }
+
+    public void insert(String word){
+        word = word.toLowerCase();
+        Node currNode = root;
+        for(int i=0; i<word.length();i++) {
+            int idx = word.charAt(i) - 'a';
+
+            if(currNode.children[idx] == null){
+                currNode.children[idx] = new Node();
+            }
+
+            if(i == word.length()-1) {
+                currNode.children[idx].eow = true;
+            }
+        }
+    }
+
+    public boolean search(String word){
+        word = word.toLowerCase();
+        Node currNode = root;
+        for(int i=0; i<word.length();i++) {
+            int idx = word.charAt(i) - 'a';
+
+            if(currNode.children[idx] == null){
+                return false;
+            }
+
+            if(i == word.length()-1 && currNode.children[idx].eow == false) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
