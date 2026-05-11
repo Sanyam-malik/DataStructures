f1c3acedf19fc927660d1d35270e28acf5dbe91b
sammy <samarora1051@gmail.com>
2026-05-11 10:26:23 +0530

    Add src/Arrays/AddDigits.java

---
+++ src/Arrays/AddDigits.java
@@ -0,0 +1,25 @@
+/*
+<metadata>
+  <name>Add Digits</name>
+  <url>https://leetcode.com/problems/add-digits</url>
+  <status>Completed</status>
+</metadata>
+*/
+class Solution {
+    public int addDigits(int num) {
+        int ans=0;
+        if(num == 0){
+            return 0;
+        }
+        while (ans == 0 || ans >=10){
+            ans = 0;
+            while(num > 0){
+                ans += num%10;
+                num = num/10;
+            }
+            num = ans;
+            
+        }
+        return ans;
+    }
+}
