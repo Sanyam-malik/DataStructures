453c48d0fe68082c2d3340ec55a18a0ffeefc6cc
sammy <samarora1051@gmail.com>
2026-05-11 10:47:26 +0530

    Add src/Arrays/CommonFactors.java

---
+++ src/Arrays/CommonFactors.java
@@ -0,0 +1,12 @@
+
+//https://leetcode.com/problems/number-of-common-factors/
+class Solution {
+    public int commonFactors(int a, int b) {
+        int min = Math.min(a, b);
+        int count = 1; // Because of 1 as our first common factor
+        for(int i = 2; i<= min; i++) {
+            if(a%i == 0 && b%i == 0) count++;
+        }
+        return count;
+    }
+}
