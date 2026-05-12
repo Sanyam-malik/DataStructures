ff1a5115ae430a58d930ada01e20f21b0714d2e3
sammy <samarora1051@gmail.com>
2026-05-13 02:17:03 +0530

    Update src/Arrays/GCDOfNumber.java

--- src/Arrays/GCDOfNumber.java
+++ src/Arrays/GCDOfNumber.java
@@ -1,4 +1,13 @@
-//https://www.geeksforgeeks.org/problems/gcd-of-two-numbers3459/1
+//
+/*
+<metadata>
+  <name>GCD of two numbers</name>
+  <url>https://www.geeksforgeeks.org/problems/gcd-of-two-numbers3459/1</url>
+  <status>Completed</status>
+  <level>Easy</level>
+  <date>2026-05-11</date>
+</metadata>
+*/
 class Solution {
     public static int gcd(int a, int b) {
         // code here
