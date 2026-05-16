87551b3ba2d5cb969376e9a1c652f3f753573c9a
Sam Arora <samarora1051@gmail.com>
2026-05-17 02:29:03 +0530

    Added/Updated File: /tmp/repositories/codebranch/src/Arrays/add_digits.java

--- src/Arrays/add_digits.java
+++ src/Arrays/add_digits.java
@@ -1,7 +1,7 @@
 /*
 <metadata>
 <name>Add Digits</name>
-	<description>Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+<description>Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
 
 Example 1:
 
@@ -22,12 +22,12 @@ Constraints:
 0 <= num <= 231 - 1
 
 Follow up: Could you do it without any loop/recursion in O(1) runtime?</description>
-	<url>https://leetcode.com/problems/add-digits</url>
-	<status>Completed</status>
-	<date>2026-05-17</date>
-	<level>Easy</level>
-	<countinclusion>true</countinclusion>
-	<sheetitemstatus>true</sheetitemstatus>
+<url>https://leetcode.com/problems/add-digits</url>
+<status>Completed</status>
+<date>2026-05-17</date>
+<level>Easy</level>
+<countinclusion>true</countinclusion>
+<sheetitemstatus>true</sheetitemstatus>
 </metadata>
 */
 class Solution {
