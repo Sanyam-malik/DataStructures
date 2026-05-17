dedc86a1c7426a9af303001e0de15000c6b719c3
sammy <samarora1051@gmail.com>
2026-05-18 05:23:20 +0530

    Update src/Strings/valid_anagram.java

--- src/Strings/valid_anagram.java
+++ src/Strings/valid_anagram.java
@@ -2,81 +2,22 @@
 <metadata>
     <name>Valid Anagram</name>
     <url>https://leetcode.com/problems/valid-anagram/description/</url>
-    <description>Given two strings s and t, return true if t is an  of s, and false otherwise.
-
-
-
-
-
- 
-
-
-
+    <description>Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 Example 1:
 
-
-
-
-
-
-
 Input: s = "anagram", t = "nagaram"
-
-
-
-
-
 Output: true
-
-
-
-
-
-
 
 Example 2:
 
-
-
-
-
-
-
 Input: s = "rat", t = "car"
-
-
-
-
-
 Output: false
-
-
-
-
-
-
-
- 
-
-
-
 
 Constraints:
 
-
-
-
 1 <= s.length, t.length <= 5 * 104
 s and t consist of lowercase English letters.
-
-
-
-
- 
-
-
-
 
 Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?</description>
     <notes></notes>
