e461a8c2c267b5d42cc8ff3847916207f5a29b65
Sam Arora <samarora1051@gmail.com>
2026-05-18 03:39:25 +0530

    Added/Updated File: /var/folders/x9/vb2wfjcj2lz33p0d3c8hy1580000gn/T/repositories/codebranch/src/Arrays/add_digits.java

---
+++ src/Arrays/add_digits.java
@@ -0,0 +1,48 @@
+/*
+<metadata>
+<name>Add Digits</name>
+<description>Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+
+Example 2:
+
+Input: num = 0
+Output: 0
+
+Constraints:
+
+0 <= num <= 231 - 1
+
+Follow up: Could you do it without any loop/recursion in O(1) runtime?</description>
+<url>https://leetcode.com/problems/add-digits</url>
+<status>Completed</status>
+<date>2026-05-10</date>
+<level>Easy</level>
+</metadata>
+*/
+class Solution {
+    public int addDigits(int num) {
+        int ans=0;
+        if(num == 0){
+            return 1;
+        }
+        while (ans == 0 || ans >=10){
+            ans = 0;
+            while(num > 0){
+                ans += num%10;
+                num = num/10;
+            }
+            num = ans;
+            
+        }
+        return ans;
+    }
+}
